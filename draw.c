015b112b5a69ff45cbd80c42491b81d96544f167
Piera Falcone <pierafalcone01@gmail.com>
2019-01-22 12:34:09 +0100

    added system info and fixed quit error

--- draw.c
+++ draw.c
@@ -7,16 +7,25 @@ int main(int argc, char **argv)
 
     SDL_Window *window = SDL_CreateWindow("OpenGL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 1024, 0);
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
+
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
     gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+
+    SDL_Log("GL_VENDOR: %s\n", glGetString(GL_VENDOR));
+    SDL_Log("GL_RENDERER: %s\n", glGetString(GL_RENDERER));
+    SDL_Log("GL_VERSION: %s \n", glGetString(GL_VERSION));
+    SDL_Log("GL_SHADING_LANGUAGE_VERSION: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
     for (;;)
     {
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
-            if (event.type == SDL_Quit)
+            if (event.type == SDL_QUIT)
                 return 0;
         }
 
