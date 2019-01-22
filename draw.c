ede152f4486845a8e5c44ef375c998c2764378b6
Giorgio Pomettini <giorgio.freespire@gmail.com>
2019-01-22 13:26:58 +0100

    Added shader compilation

--- draw.c
+++ draw.c
@@ -1,6 +1,60 @@
 #include <SDL2/SDL.h>
 #include <glad/glad.h>
 
+GLuint compile_shader(GLenum shader_type, const char *filename)
+{
+    SDL_RWops *rw = SDL_RWFromFile(filename, "rb");
+    if (!rw)
+    {
+        SDL_Log("Unable to open file");
+        exit(1);
+    }
+
+    size_t file_len = SDL_RWsize(rw);
+    char *source = SDL_malloc(file_len + 1);
+    if (!source)
+    {
+        SDL_Log("Unable to allocate memory");
+        exit(1);
+    }
+
+    if (SDL_RWread(rw, source, 1, file_len) != file_len)
+    {
+        SDL_Log("Unable to read file");
+        exit(1);
+    }
+
+    source[file_len] = 0;
+
+    SDL_RWclose(rw);
+
+    GLuint shader = glCreateShader(shader_type);
+    glShaderSource(shader, 1, (const GLchar **)&source, (GLint *)&file_len);
+    glCompileShader(shader);
+
+    SDL_free(source);
+
+    GLint compile_status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_status);
+    if (compile_status == GL_FALSE)
+    {
+        GLint log_size;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_size);
+        char *error_log = SDL_malloc(log_size + 1);
+        if (!error_log)
+        {
+            SDL_Log("Unable to allocate memory for log");
+            exit(1);
+        }
+        glGetShaderInfoLog(shader, log_size, &log_size, error_log);
+        error_log[log_size] = 0;
+        SDL_Log("Shader compile error: %s", error_log);
+        exit(1);
+    }
+
+    return shader;
+}
+
 int main(int argc, char **argv)
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -11,8 +65,10 @@ int main(int argc, char **argv)
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
 
+    // Create opengl context with the major/minor specified with SetAttribute
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
+    // Load GL symbols
     gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
 
     SDL_Log("GL_VENDOR: %s\n", glGetString(GL_VENDOR));
@@ -20,10 +76,35 @@ int main(int argc, char **argv)
     SDL_Log("GL_VERSION: %s \n", glGetString(GL_VERSION));
     SDL_Log("GL_SHADING_LANGUAGE_VERSION: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
+    // Set the current clear color
     glClearColor(1.0, 0.5, 0.7, 1);
 
+    // Allocate 1 VAO (in GPU)
     GLuint vao;
     glGenVertexArrays(1, &vao);
+    // Vind the VAO (in GPU), now it is the active one
+    glBindVertexArray(vao);
+
+    // Create a new program/pipeline (in GPU)
+    GLuint program = glCreateProgram();
+
+    GLuint vertex_shader = compile_shader(GL_VERTEX_SHADER, "vertex.glsl");
+    GLuint fragment_shader = compile_shader(GL_VERTEX_SHADER, "frag.glsl");
+
+    // Attach the shaders to the program
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+
+    // Link the program (now is ready)
+    glLinkProgram(program);
+
+    glDetachShader(program, vertex_shader);
+    glDetachShader(program, fragment_shader);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    glUseProgram(program);
 
     for (;;)
     {
