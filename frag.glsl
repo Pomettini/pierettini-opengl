a2e6d19c299f3e63950bdf03ae0ccce53fef61d1
Piera Falcone <pierafalcone01@gmail.com>
2019-01-24 12:23:12 +0100

    random commit

--- frag.glsl
+++ frag.glsl
@@ -4,9 +4,14 @@ out vec4 color;
 
 in vec3 light_vertex;
 in vec3 light_normal;
+in vec2 uv;
+
+uniform sampler2D tex_base_color;
 
 void main()
 {
+    vec3 base_color = texture(tex_base_color, vec2(uv.x, 1 - uv.y)).xyz;
+    
     float lambert = clamp(dot(light_vertex, light_normal), 0, 1);
-    color = vec4(vec3(1, 1, 1) * lambert, 1);
+    color = vec4(vertex_color * lambert, 1);
 }
