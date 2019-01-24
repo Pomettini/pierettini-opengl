f5ccb52e70d12006c85988a5416f3072fd95c298
Piera Falcone <pierafalcone01@gmail.com>
2019-01-24 08:47:43 +0100

    now it works

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@ GLuint compile_shader(GLenum shader_type, const char *filename)
     SDL_RWops *rw = SDL_RWFromFile(filename, "rb");
     if (!rw)
     {
-        SDL_Log("Unable to open file");
+        SDL_Log("unable to open file");
         exit(1);
     }
 
@@ -14,13 +14,13 @@ GLuint compile_shader(GLenum shader_type, const char *filename)
     char *source = SDL_malloc(file_len + 1);
     if (!source)
     {
-        SDL_Log("Unable to allocate memory");
+        SDL_Log("unable to allocate memory");
         exit(1);
     }
 
     if (SDL_RWread(rw, source, 1, file_len) != file_len)
     {
-        SDL_Log("Unable to read file");
+        SDL_Log("unable to read file");
         exit(1);
     }
 
@@ -29,7 +29,9 @@ GLuint compile_shader(GLenum shader_type, const char *filename)
     SDL_RWclose(rw);
 
     GLuint shader = glCreateShader(shader_type);
+    // assign the shader code
     glShaderSource(shader, 1, (const GLchar **)&source, (GLint *)&file_len);
+    // compile the shader
     glCompileShader(shader);
 
     SDL_free(source);
@@ -43,12 +45,13 @@ GLuint compile_shader(GLenum shader_type, const char *filename)
         char *error_log = SDL_malloc(log_size + 1);
         if (!error_log)
         {
-            SDL_Log("Unable to allocate memory for log");
+            SDL_Log("unable to allocate memory for log");
             exit(1);
         }
         glGetShaderInfoLog(shader, log_size, &log_size, error_log);
         error_log[log_size] = 0;
-        SDL_Log("Shader compile error: %s", error_log);
+        SDL_Log("shader compile error: %s", error_log);
+        SDL_free(error_log);
         exit(1);
     }
 
@@ -63,26 +66,26 @@ int main(int argc, char **argv)
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
-    // Create opengl context with the major/minor specified with SetAttribute
+    // create opengl context with the major/minor specified with SetAttribute
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
-    // Load GL symbols
+    // load GL symbols
     gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
 
-    SDL_Log("GL_VENDOR: %s\n", glGetString(GL_VENDOR));
-    SDL_Log("GL_RENDERER: %s\n", glGetString(GL_RENDERER));
-    SDL_Log("GL_VERSION: %s \n", glGetString(GL_VERSION));
-    SDL_Log("GL_SHADING_LANGUAGE_VERSION: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+    SDL_Log("GL_VENDOR: %s", glGetString(GL_VENDOR));
+    SDL_Log("GL_RENDERER: %s", glGetString(GL_RENDERER));
+    SDL_Log("GL_VERSION: %s", glGetString(GL_VERSION));
+    SDL_Log("GL_SHADING_LANGUAGE_VERSION: %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
-    // Set the current clear color
-    glClearColor(1.0, 0.5, 0.7, 1);
+    // set the current clear color
+    glClearColor(1, 0, 0, 1);
 
-    // Allocate 1 VAO (in GPU)
+    // allocate 1 VAO (in GPU)
     GLuint vao;
     glGenVertexArrays(1, &vao);
-    // Vind the VAO (in GPU), now it is the active one
+    // bind the VAO (in GPU). now it is the active one
     glBindVertexArray(vao);
 
     GLuint vbo[2];
@@ -106,17 +109,17 @@ int main(int argc, char **argv)
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    // Create a new program/pipeline (in GPU)
+    // create a new program/pipeline (in GPU)
     GLuint program = glCreateProgram();
 
     GLuint vertex_shader = compile_shader(GL_VERTEX_SHADER, "vertex.glsl");
-    GLuint fragment_shader = compile_shader(GL_VERTEX_SHADER, "frag.glsl");
+    GLuint fragment_shader = compile_shader(GL_FRAGMENT_SHADER, "frag.glsl");
 
-    // Attach the shaders to the program
+    // attach the sahders to the program
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
 
-    // Link the program (now is ready)
+    // linke the program (now is ready)
     glLinkProgram(program);
 
     glDetachShader(program, vertex_shader);
@@ -125,6 +128,7 @@ int main(int argc, char **argv)
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
+    // set the currently active program/pipeline
     glUseProgram(program);
 
     GLint x_uniform = glGetUniformLocation(program, "x");
@@ -144,13 +148,13 @@ int main(int argc, char **argv)
             if (event.type == SDL_KEYDOWN)
             {
                 if (event.key.keysym.sym == SDLK_RIGHT)
-                    triangle_x += 0.01;
+                    triangle_x += 0.1;
                 if (event.key.keysym.sym == SDLK_LEFT)
-                    triangle_x -= 0.01;
+                    triangle_x -= 0.1;
                 if (event.key.keysym.sym == SDLK_UP)
-                    triangle_y += 0.01;
+                    triangle_y += 0.1;
                 if (event.key.keysym.sym == SDLK_DOWN)
-                    triangle_y -= 0.01;
+                    triangle_y -= 0.1;
             }
         }
 
